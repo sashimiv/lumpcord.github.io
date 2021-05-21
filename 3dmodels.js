74e9830ebabfb841a13914634278c82b25f821ac
sashimiv <74048469+sashimiv@users.noreply.github.com>
2021-05-22 01:57:52 +0300

    Update 3dmodels.js
    ХОЧУ КАК У ЭПЛ

--- 3dmodels.js
+++ 3dmodels.js
@@ -1,1 +1,3 @@
 
+ /echo 1
+<video id="video-hero" data-inline-media="" data-inline-media-portrait-class-element="html" data-inline-media-orientation-mask="M" data-inline-media-plugins="ViewportSourcePro" data-inline-media-basepath="/105/media/ww/iphone-12-pro/2020/e70ffbd8-50f1-40f3-ac36-0f03a15ac314/anim/hero/non_5g/" preload="" muted="" aria-hidden="true" src="/105/media/ww/iphone-12-pro/2020/e70ffbd8-50f1-40f3-ac36-0f03a15ac314/anim/hero/non_5g/large.mp4"></video>
